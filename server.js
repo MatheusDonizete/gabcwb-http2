b54452be53558d262701a390dc9955a63376da99
Matheus Donizete <matheus@strikexii.com>
2017-04-22 10:27:44 -0300

    removing useless lines

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const options = {
 };
 
 const server = spdy.createServer(options, app);
+/*const server = spdy.createServer({
+    spdy: {
+        plain: true,
+        ssl: false
+    }
+});*/
 app.get('/', (req, res) => {
     const pageHTML = fs.readFileSync(`${__dirname}/static/index.html`);
     const jsOptions = {
@@ -22,28 +28,19 @@ app.get('/', (req, res) => {
             'content-type': 'application/javascript'
         }
     };
-
-    let stream = res.push(`/app.js`, jsOptions);
-
-    stream.on('error', () => {});
-    stream.write(`alert('HTTP/2 Server Push Habilitado :)');`); //Método write() mantém a resposta aberta
-    stream.end(fs.readFileSync(`${__dirname}/static/js/app.js`)); // Método end() fecha automaticamente a conexão
+    if (res.push) {
+        let stream = res.push(`/app.js`, jsOptions);
+        stream.on('error', () => {});
+        stream.write(`alert('HTTP/2 Server Push Habilitado :)');`); //Método write() mantém a resposta aberta
+        stream.end(fs.readFileSync(`${__dirname}/static/js/app.js`)); // Método end() fecha automaticamente a conexão   
+    }
 
     res.set('Content-Type', 'text/html');
     res.send(new Buffer(pageHTML));
 });
-
-app.use(function(req, res, next) {
-    const matchUrl = '/styles/fonts';
-    if (req.url.substring(0, matchUrl.length) === matchUrl) {
-        res.setHeader('content-type', 'font');
-    }
-    return next();
-});
-
 app.use(express.static(`${__dirname}/static`));
 
-server.listen(process.env.PORT || 443, () => {
+server.listen(process.env.PORT || 3000, () => {
     let host = server.address().address;
     let port = server.address().port;
     console.log(`Listening at: https://${host}:${port}`);
